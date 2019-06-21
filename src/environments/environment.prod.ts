e3b68c260611d0e56ad1c72290ec54b2b3aa5ebb
katarzyna-sidur <katarzyna.sidur@gmail.com>
2019-06-21 11:34:02 +0200

    fi environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: 'AIzaSyCGsdln34RWrOS-ZhSbEskj-FajpAXNFpo',
+        authDomain: 'clothes-shop-fe8d1.firebaseapp.com',
+        databaseURL: 'https://clothes-shop-fe8d1.firebaseio.com',
+        projectId: 'clothes-shop-fe8d1',
+        storageBucket: 'clothes-shop-fe8d1.appspot.com',
+        messagingSenderId: '998450370631'
+    }
 };
